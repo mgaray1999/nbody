124a7275417dc7fd6decd42ee427bdb287282a33
Miguel Garay <meg89@duke.edu>
2018-01-27 14:22:01 -0600

    "update"

---
+++ src/Body.java
@@ -0,0 +1,16 @@
+
+public class Body {
+	double myXPos; //current x position
+	double myYPos; //current y position
+	double myXVel; //current velocity in x direction
+	double myYVel; //current velocity in y direction
+	double myMass; //mass of body
+	String myFileName; //file name (in images folder)
+	
+	public Body(double xp, double yp, double xv, double yv, double mass, String filename) {
+		
+	}
+	public Body(Body p) {
+		
+	}
+}
