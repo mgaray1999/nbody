4411023baef879a366b52399708b1a98076d7830
Miguel Garay <meg89@duke.edu>
2018-01-29 17:05:24 -0600

    submission

--- src/Body.java
+++ src/Body.java
@@ -1,4 +1,4 @@
-
+import java.math.*;
 public class Body {
 	double myXPos; //current x position
 	double myYPos; //current y position
@@ -8,9 +8,88 @@ public class Body {
 	String myFileName; //file name (in images folder)
 	
 	public Body(double xp, double yp, double xv, double yv, double mass, String filename) {
-		
+		myXPos = xp;
+		myYPos = yp;
+		myXVel = xv;
+		myYVel = yv;
+		myMass = mass;
+		myFileName = filename;
 	}
+	
 	public Body(Body p) {
-		
+		myXPos = p.myXPos;
+		myYPos = p.myYPos;
+		myXVel = p.myXVel;
+		myYVel = p.myYVel;
+		myMass = p.myMass;
+		myFileName = p.myFileName;
 	}
+	
+	public void draw() {
+		StdDraw.picture(myXPos, myYPos, "images/"+myFileName);
+	}
+	
+	public double calcDistance(Body p) {
+		double xpos = this.myXPos;
+		double ypos = this.myYPos;
+		double distance = (p.myXPos - xpos)*(p.myXPos - xpos) +  
+				(p.myYPos - ypos)*(p.myYPos - ypos);
+		return Math.sqrt(distance);
+	}
+	
+	public double calcForceExertedBy(Body p) {
+		double g = 6.67 * Math.pow(10, -11); 
+		double force = g*(p.myMass * this.myMass)/Math.pow(this.calcDistance(p), 2);
+		return force;
+	}
+	
+	public double calcForceExertedByX(Body p) {
+		double xpos = this.myXPos;
+		double dist = this.calcDistance(p);
+		double xforce = this.calcForceExertedBy(p) * (p.myXPos - xpos) / dist;
+		return xforce;
+	}
+	
+	public double calcForceExertedByY(Body p) {
+		double ypos = this.myYPos;
+		double dist = this.calcDistance(p);
+		double yforce = this.calcForceExertedBy(p) * (p.myYPos - ypos) / dist;
+		return yforce;
+	}
+	
+	public double calcNetForceExertedByX(Body[] p) {
+		double sum = 0;
+		for (int i = 0; i < p.length; i++) {
+			if (! p[i].equals(this)) {
+				sum += calcForceExertedByX(p[i]);
+			}
+		}
+		return sum;
+	}
+	
+	public double calcNetForceExertedByY(Body[] p) {
+		double sum = 0;
+		for (int i = 0; i < p.length; i++) {
+			if (! p[i].equals(this)) {
+				sum += calcForceExertedByY(p[i]);
+			}
+		}
+		return sum;
+	}
+	
+	public void update(double seconds, double xforce, double yforce) {
+		double ax = xforce / this.myMass;
+		double ay = yforce / this.myMass;
+		double xvelo = this.myXVel + seconds * ax;
+		double yvelo = this.myYVel + seconds * ay;
+		double xpos = this.myXPos + seconds * xvelo;
+		double ypos = this.myYPos + seconds * yvelo;
+		
+		this.myXVel = xvelo;
+		this.myYVel = yvelo;
+		this.myXPos = xpos;
+		this.myYPos = ypos;
+	}
+	
+	
 }
--- src/NBody.java
+++ src/NBody.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class NBody {
 	public static void main(String[] args) {
-		double totalTime = 157788000.0;
-		double dt = 25000.0;
+		double totalTime = Math.pow(10, 9); //157788000.0; 
+		double dt = Math.pow(10, 6); //25000.0;
 		String pfile = "data/planets.txt";
 		if (args.length > 2) {
 			totalTime = Double.parseDouble(args[0]);
@@ -14,12 +14,55 @@ public class NBody {
 		}
 
 		// Read simulation data from file
-		Body[] planets = null; // readBodies(pfile);
-		double radius = 0.0; // readRadius(pfile);
+		Body[] planets = readBodies(pfile); // readBodies(pfile);
+		double radius = readRadius(pfile); // readRadius(pfile);
 
 		// TODO Draw the background
+		StdDraw.setScale(-radius,radius);
+		StdDraw.picture(0, 0, "images/starfield.jpg");
+		
+		//draws all the bodies
+		for (int i = 0; i < planets.length; i++) {
+			planets[i].draw();
+		}
 
 		// TODO Animate the simulation from time 0 until totalTime
+		double time = 0.0;
+		for (double i = time; i < totalTime; i += dt) {
+			double[] xForces = new double[planets.length];
+			double[] yForces = new double[planets.length];
+			for (int z = 0; z < planets.length; z++) {
+				double netx = planets[z].calcNetForceExertedByX(planets);
+				double nety = planets[z].calcNetForceExertedByY(planets);
+				xForces[z] = netx;
+				yForces[z] = nety;
+			}
+			for (int x = 0; x < planets.length; x++) {
+				planets[x].update(dt, xForces[x], yForces[x]);
+			}
+			
+			//draws the background
+			StdDraw.setScale(-radius,radius);
+			StdDraw.picture(0, 0, "images/starfield.jpg");
+			
+			//draws all the bodies
+			for (int y = 0; y < planets.length; y++) {
+				planets[y].draw();
+			}
+			StdDraw.show(10);
+			if (time == 1000000 || time == 2000000) {
+				System.out.printf("%d\n", planets.length);
+				System.out.printf("%.2e\n", radius);
+				for (int w = 0; w < planets.length; w++) {
+					System.out.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n", 
+									  planets[w].myXPos, planets[w].myYPos,
+									  planets[w].myXVel, planets[w].myYVel, 
+									  planets[w].myMass, planets[w].myFileName);
+				}
+			}
+			time += dt;
+			
+		}
 
 		// Print final positions of planets
 		System.out.printf("%d\n", planets.length);
@@ -29,6 +72,47 @@ public class NBody {
 							  planets[i].myXPos, planets[i].myYPos,
 							  planets[i].myXVel, planets[i].myYVel, 
 							  planets[i].myMass, planets[i].myFileName);
+		}
+	} // end main method
+	
+	public static double readRadius(String fname) {
+		double radius = 0.0;
+		try {
+			Scanner scan = new Scanner(new File(fname));
+			scan.nextInt();
+			radius = scan.nextDouble();
+			scan.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found.");
+			System.exit(0);
+		}
+		return radius;
+	}
+	
+	public static Body[] readBodies(String fname) {
+		try {
+			Scanner scan = new Scanner(new File(fname));
+			int numbodies = scan.nextInt(); // reads the number of planets
+			scan.nextDouble(); // reads and moves past the radius
+			Body[] p = new Body[numbodies];
+			for (int i = 0; i < numbodies; i++) {
+				double xPos = scan.nextDouble();
+				double yPos = scan.nextDouble();
+				double xVel = scan.nextDouble();
+				double yVel = scan.nextDouble();
+				double myMass = scan.nextDouble();
+				String myFileName = scan.next();
+				
+				Body q = new Body(xPos, yPos, xVel, yVel, myMass, myFileName);
+				p[i] = q;
+			}
+			scan.close();
+			return p;
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found.");
+			Body[] a = null;
+			System.exit(0);
+			return a;
 		}
 	}
 }
